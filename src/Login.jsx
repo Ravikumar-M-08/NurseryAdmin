97e7be0c217ef79e3ae816a4295394bc0dbd6294
Ravikumar <ravikumar936318@gmail.com>
2025-07-14 12:06:57 +0530

    change

--- src/Login.jsx
+++ src/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     const handleSubmit = (e) => {
         e.preventDefault();
         if (username == formData.name && userpass == formData.password) {
-            window.location.href = "https://nurseryadmin.netlify.app/itemlist"
+            window.location.href = "/itemlist"
         }
         else {
             setError("Username and Password Not Match");
