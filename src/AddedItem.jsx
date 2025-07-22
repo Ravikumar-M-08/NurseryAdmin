cc9bfe494ea80eeebbf9a490165b32857bd35abb
Ravikumar <ravikumar936318@gmail.com>
2025-07-22 10:50:05 +0530

    change

---
+++ src/AddedItem.jsx
@@ -0,0 +1,10 @@
+function Addeditem() {
+    return (
+        <>
+        <div className="container">
+            <h2>Success</h2>
+        </div>
+        </>
+        );
+}
+export default Addeditem;
